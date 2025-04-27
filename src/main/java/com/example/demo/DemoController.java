4da359f8a7e44b9c0a164b617d3b04010501166d
Libor Cerny <libor.cerny@email.com>
2025-04-27 22:12:43 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonAnyFormatVisitor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+}
