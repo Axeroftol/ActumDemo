cfdb342db772c7b2d7a54a8a5bf8e5fb050ec72e
Libor Cerny <libor.cerny@email.com>
2025-04-29 11:39:11 +0200

    Removed unused library import

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonAnyFormatVisitor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
