a6f6ecca9bff5922bbde019b7ea8f3bd8ac4b525
Libor Cerny <libor.cerny@email.com>
2025-04-29 08:15:21 +0200

    Added POST method /submit

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonAnyFormatVisitor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +16,10 @@ public class DemoController {
     @GetMapping("/hello2")
     public String helloGrohe() {
         return "Hello Grohe!";
+    }
+
+    @PostMapping("/submit")
+    public String submitData(@RequestBody String body) {
+        return "Received: " + body;
     }
 }
